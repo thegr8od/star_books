ab734b49774d1842d78b7281e3e2eb08a8adc630
joehyejeong <gpwjdwngml@naver.com>
2025-02-03 00:16:42 +0900

    feat: 로그인 기능 추가

---
+++ frontend/src/data/users.jsx
@@ -0,0 +1,14 @@
+export const users = [
+    {
+      email: "john@example.com",
+      password: "12345"
+    },
+    {
+      email: "jane@company.com",
+      password: "password123"
+    },
+    {
+      email: "test@test.com",
+      password: "testpass"
+    }
+  ];
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,8 +1,13 @@
+import Login from "../Auth/Login";
 function Home() {
     return (
-      <>
-        <p>Home!</p>
-      </>
+      <div className="flex flex-col justify-center items-center">
+        <div className="pt-[150px] flex flex-col items-center">
+          <h1 className="text-2xl font-medium text-white/75 mb-2">별에 별 일</h1>
+          <p className="text-sm font-thin text-white/50 ">하늘에 담은 당신의 이야기</p>
+        </div>
+        <Login/>
+      </div>
     );
   }
   
