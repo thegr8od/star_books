2674a1583167318decfe82bc678c119be71db37b
joehyejeong <gpwjdwngml@naver.com>
2025-02-06 18:03:24 +0900

    feat: 로그인 axios 기능 추가 (디자인 변경)

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,14 +1,21 @@
-import Login from "../Auth/Login";
-function Home() {
-    return (
-      <div className="flex flex-col justify-center items-center">
-        <div className="pt-[150px] flex flex-col items-center">
-          <h1 className="text-2xl font-medium text-white/75 mb-2">별에 별 일</h1>
-          <p className="text-sm font-thin text-white/50 ">하늘에 담은 당신의 이야기</p>
-        </div>
-        <Login/>
-      </div>
-    );
-  }
-  
-  export default Home
+import { useEffect, useState } from 'react';
+import HomeBackground from './HomeBackground';
+import LoginHome from './LoginHome';
+
+const Home = () => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+  useEffect(() => {
+    // 로그인 상태 확인
+    const accessToken = sessionStorage.getItem('accessToken');
+    setIsLoggedIn(!!accessToken); // accessToken이 있으면 true, 없으면 false
+  }, []); // 컴포넌트 마운트 시 한 번만 실행
+
+  return (
+    <>
+      {isLoggedIn ? <LoginHome /> : <HomeBackground />}
+    </>
+  );
+};
+
+export default Home;
--- frontend/src/pages/Radio/RadioList.jsx
+++ frontend/src/pages/Radio/RadioList.jsx
@@ -1,7 +1,12 @@
+import LoginHome from "../Home/LoginHome";
+
 function RadioList() {
     return (
       <>
         <p>RadioList!</p>
+        <p>LoginHome 테스트</p>
+        <LoginHome/> 
+        
       </>
     );
   }
