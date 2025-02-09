a2be8230cc553fbd8515a7c4fcfc756cae8cf02a
김은영 <kimeunyoung-0425@naver.com>
2025-02-09 16:24:32 +0900

    chore: Redux 셋팅

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,11 +1,14 @@
-import './index.css'
-import App from './App.jsx'
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom';
+import "./index.css";
+import App from "./App.jsx";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import { Provider } from "react-redux";
+import { store } from "./store/store";
 
-createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+createRoot(document.getElementById("root")).render(
+  <Provider store={store}>
+    <BrowserRouter>
       <App />
-  </BrowserRouter>
-
-)
+    </BrowserRouter>
+  </Provider>
+);
