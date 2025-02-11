63480540a993306509d224a45e3191216d6be637
권동현 <ehdgus9370@gmail.com>
2025-02-12 06:11:12 +0900

    feat: api 연동 및 member api 1차 작업

---
+++ frontend/src/api/useDiaryApi.jsx
@@ -0,0 +1,1 @@
+import useAxiosInstance from "./useAxiosInstance";
---
+++ frontend/src/constants/constants.jsx
@@ -0,0 +1,3 @@
+// API URL
+export const API_URL = "https://i12d206.p.ssafy.io/api";
+export const CONTENT_TYPE_JSON = "application/json";
