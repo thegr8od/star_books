53e08087a38a48f3939099fc1a9df3627d916044
김은영 <kimeunyoung-0425@naver.com>
2025-02-12 14:01:59 +0900

    feat: 별자리 3D버튼과 컴포넌트 연결

--- frontend/src/pages/Diary/Diary.jsx
+++ frontend/src/pages/Diary/Diary.jsx
@@ -1,14 +1,16 @@
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
 import Layout from "../../components/Layout";
 import DiaryStars from "./DiaryStars";
 import DiaryCalendar from "./DiaryCalendar";
 import Header from "../../components/Header";
-import { KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 import Button from "../../components/Button";
 import MoodSurvey from "./MoodSurvey";
-import { Add } from "@mui/icons-material";
+import { Add, KeyboardArrowLeft, KeyboardArrowRight } from "@mui/icons-material";
 
 function Diary() {
+  const navigate = useNavigate();
+
   const [currentDate, setCurrentDate] = useState(new Date());
   const [activeTab, setActiveTab] = useState(2);
   const [showModal, setShowModal] = useState(false);
@@ -17,11 +19,7 @@ function Diary() {
   // 날짜 변경 핸들러 (이전 달)
   const handlePrevMonth = () => {
     setCurrentDate((prev) => {
-      const newDate = new Date(
-        prev.getFullYear(),
-        prev.getMonth() - 1,
-        prev.getDate()
-      );
+      const newDate = new Date(prev.getFullYear(), prev.getMonth() - 1, prev.getDate());
       // axios 요청
       return newDate;
     });
@@ -30,11 +28,7 @@ function Diary() {
   // 날짜 변경 핸들러 (다음 달)
   const handleNextMonth = () => {
     setCurrentDate((prev) => {
-      const newDate = new Date(
-        prev.getFullYear(),
-        prev.getMonth() + 1,
-        prev.getDate()
-      );
+      const newDate = new Date(prev.getFullYear(), prev.getMonth() + 1, prev.getDate());
       // axios 요청
       return newDate;
     });
@@ -42,19 +36,11 @@ function Diary() {
 
   // 날짜 포맷 함수
   const formatDate = {
-    toDisplay: (date) =>
-      `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(
-        2,
-        "0"
-      )}월`,
-    toAPI: (date) =>
-      `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(
-        2,
-        "0"
-      )}-${String(date.getDate()).padStart(2, "0")}`,
+    toDisplay: (date) => `${date.getFullYear()}년 ${String(date.getMonth() + 1).padStart(2, "0")}월`,
+    toAPI: (date) => `${date.getFullYear()}-${String(date.getMonth() + 1).padStart(2, "0")}-${String(date.getDate()).padStart(2, "0")}`,
   };
   //console.log(currentDate);
-  //Tue Feb 11 2025 00:00:00 
+  //Tue Feb 11 2025 00:00:00
 
   return (
     <Layout>
@@ -70,49 +56,24 @@ function Diary() {
             </button>
           }
           rightChild={
-            <button
-              onClick={handleNextMonth}
-              disabled={
-                new Date(
-                  currentDate.getFullYear(),
-                  currentDate.getMonth() + 1
-                ) > today
-              }
-            >
+            <button onClick={handleNextMonth} disabled={new Date(currentDate.getFullYear(), currentDate.getMonth() + 1) > today}>
               <KeyboardArrowRight />
             </button>
           }
         />
 
         {/* 메인 */}
-        <div className="flex-1">
-          {activeTab === 1 ? (
-            <DiaryStars />
-          ) : (
-            <DiaryCalendar currentMonth={currentDate} />
-          )}
-        </div>
+        <div className="flex-1">{activeTab === 1 ? <DiaryStars /> : <DiaryCalendar currentMonth={currentDate} />}</div>
 
         {/* 버튼 */}
         <div className="flex justify-center items-center mt-3 space-x-8">
-          <Button
-            text="나의 별"
-            type={`${activeTab === 1 ? "NEXT" : "PREV"}`}
-            onClick={() => setActiveTab(1)}
-            className="w-24 h-9 text-sm"
-          />
-          <Button
-            text={<Add />}
-            type="DEFAULT"
-            className="h-10 w-10 rounded-full border border-white bg-transparent hover:bg-transparent"
-            onClick={() => setShowModal(true)}
-          />
-          <Button
-            text="캘린더"
-            type={`${activeTab === 2 ? "NEXT" : "PREV"}`}
-            onClick={() => setActiveTab(2)}
-            className="w-24 h-9 text-sm"
-          />
+          <Button text="나의 별" type={`${activeTab === 1 ? "NEXT" : "PREV"}`} onClick={() => setActiveTab(1)} className="w-24 h-9 text-sm" />
+          {activeTab === 1 ? (
+            <Button text="3D" type="DEFAULT" className="h-10 w-10 rounded-full border border-white bg-transparent hover:bg-transparent" onClick={() => navigate("/universe")} />
+          ) : (
+            <Button text={<Add />} type="DEFAULT" className="h-10 w-10 rounded-full border border-white bg-transparent hover:bg-transparent" onClick={() => setShowModal(true)} />
+          )}
+          <Button text="캘린더" type={`${activeTab === 2 ? "NEXT" : "PREV"}`} onClick={() => setActiveTab(2)} className="w-24 h-9 text-sm" />
         </div>
       </div>
 
