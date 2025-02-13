61c3e4c71a383fb3505e94abd87d1abb6e99a7c0
joehyejeong <gpwjdwngml@naver.com>
2025-02-13 12:57:32 +0900

    feat: sessionStorage를 localStorage로 변경

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -1,21 +1,17 @@
-import { useEffect, useState } from 'react';
-import HomeBackground from './HomeBackground';
-import LoginHome from './LoginHome';
+import { useEffect, useState } from "react";
+import HomeBackground from "./HomeBackground";
+import LoginHome from "./LoginHome";
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   useEffect(() => {
     // 로그인 상태 확인
-    const accessToken = sessionStorage.getItem('accessToken');
+    const accessToken = localStorage.getItem("accessToken");
     setIsLoggedIn(!!accessToken); // accessToken이 있으면 true, 없으면 false
   }, []); // 컴포넌트 마운트 시 한 번만 실행
 
-  return (
-    <>
-      {isLoggedIn ? <LoginHome /> : <HomeBackground />}
-    </>
-  );
+  return <>{isLoggedIn ? <LoginHome /> : <HomeBackground />}</>;
 };
 
 export default Home;
