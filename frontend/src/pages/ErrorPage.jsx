eea4a778dab76e090f3396f1168cdd48494e8300
정주은 <jooeun0807@naver.com>
2025-02-14 11:26:00 +0900

    refactor: 기존 에러페이지를 잘못된 접근 시에도 사용할 수 있도록 변경

--- frontend/src/pages/ErrorPage.jsx
+++ frontend/src/pages/ErrorPage.jsx
@@ -3,7 +3,7 @@ import Button from "../components/Button";
 import { useNavigate } from "react-router-dom";
 import errorImage from "/images/errorImage.png";
 
-function ErrorPage() {
+function ErrorPage(title = "404 ERROR", message, customStyle) {
   const navigate = useNavigate();
   const goHome = () => {
     navigate("/");
@@ -15,11 +15,16 @@ function ErrorPage() {
           <div className="w-40 h-40 mx-auto">
             <img src={errorImage} alt="에러이미지" />
           </div>
-          <h1 className="text-[50px] font-bold text-white">404 ERROR</h1>
-          <div className="space-y-4 text-white">
-            <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
-            <p>존재하지 않는 주소를 입력하셨거나</p>
-            <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
+          {/* 잘못된 접근했을 때도 이 페이지 사용할 수 있도록 수정 */}
+          <h1 className="text-[50px] font-bold text-white">{title}</h1>
+          <div className={`space-y-4 text-white ${customStyle}`}>
+            {message || (
+              <div>
+                <p>죄송합니다. 페이지를 찾을 수 없습니다.</p>
+                <p>존재하지 않는 주소를 입력하셨거나</p>
+                <p>요청하신 페이지의 주소가 변경, 삭제되어 찾을 수 없습니다.</p>
+              </div>
+            )}
           </div>
           <Button
             text={"홈으로"}
