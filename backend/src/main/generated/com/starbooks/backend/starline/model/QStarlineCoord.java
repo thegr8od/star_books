1510e88cfc4643760e94b2de19b076a2e997cbc1
김승준 <zzjoon97@naver.com>
2025-02-14 13:15:10 +0900

    feat : universe 반환 값에 xvalue,yvalue(색값) 추가 fix : @/monthly 로직 수정

--- backend/src/main/generated/com/starbooks/backend/starline/model/QStarlineCoord.java
+++ backend/src/main/generated/com/starbooks/backend/starline/model/QStarlineCoord.java
@@ -7,7 +7,6 @@ import com.querydsl.core.types.dsl.*;
 import com.querydsl.core.types.PathMetadata;
 import javax.annotation.processing.Generated;
 import com.querydsl.core.types.Path;
-import com.querydsl.core.types.dsl.PathInits;
 
 
 /**
@@ -18,40 +17,28 @@ public class QStarlineCoord extends EntityPathBase<StarlineCoord> {
 
     private static final long serialVersionUID = 2138826815L;
 
-    private static final PathInits INITS = PathInits.DIRECT2;
-
     public static final QStarlineCoord starlineCoord = new QStarlineCoord("starlineCoord");
 
-    public final DateTimePath<java.time.LocalDateTime> createdAt = createDateTime("createdAt", java.time.LocalDateTime.class);
+    public final NumberPath<Long> endEmotionId = createNumber("endEmotionId", Long.class);
 
-    public final com.starbooks.backend.diary.model.QDiaryEmotion diaryEmotionEnd;
-
-    public final com.starbooks.backend.diary.model.QDiaryEmotion diaryEmotionStart;
+    public final NumberPath<Integer> month = createNumber("month", Integer.class);
 
     public final NumberPath<Long> starlineCoordId = createNumber("starlineCoordId", Long.class);
 
-    public final DateTimePath<java.time.LocalDateTime> updatedAt = createDateTime("updatedAt", java.time.LocalDateTime.class);
+    public final NumberPath<Long> startEmotionId = createNumber("startEmotionId", Long.class);
+
+    public final NumberPath<Integer> year = createNumber("year", Integer.class);
 
     public QStarlineCoord(String variable) {
-        this(StarlineCoord.class, forVariable(variable), INITS);
+        super(StarlineCoord.class, forVariable(variable));
     }
 
     public QStarlineCoord(Path<? extends StarlineCoord> path) {
-        this(path.getType(), path.getMetadata(), PathInits.getFor(path.getMetadata(), INITS));
+        super(path.getType(), path.getMetadata());
     }
 
     public QStarlineCoord(PathMetadata metadata) {
-        this(metadata, PathInits.getFor(metadata, INITS));
-    }
-
-    public QStarlineCoord(PathMetadata metadata, PathInits inits) {
-        this(StarlineCoord.class, metadata, inits);
-    }
-
-    public QStarlineCoord(Class<? extends StarlineCoord> type, PathMetadata metadata, PathInits inits) {
-        super(type, metadata, inits);
-        this.diaryEmotionEnd = inits.isInitialized("diaryEmotionEnd") ? new com.starbooks.backend.diary.model.QDiaryEmotion(forProperty("diaryEmotionEnd"), inits.get("diaryEmotionEnd")) : null;
-        this.diaryEmotionStart = inits.isInitialized("diaryEmotionStart") ? new com.starbooks.backend.diary.model.QDiaryEmotion(forProperty("diaryEmotionStart"), inits.get("diaryEmotionStart")) : null;
+        super(StarlineCoord.class, metadata);
     }
 
 }
