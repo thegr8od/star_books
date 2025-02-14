13ed99ecef75523fc7e0e9f3343e65cac23286da
김은영 <kimeunyoung-0425@naver.com>
2025-02-14 15:46:54 +0900

    feat: Redux-persis 사용하여 사용자 정보 관리

--- frontend/src/store/store.jsx
+++ frontend/src/store/store.jsx
@@ -1,8 +1,25 @@
 import { configureStore } from "@reduxjs/toolkit";
-import counterReducer from "./counterSlice";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage"; // 로컬 스토리지 사용
+import userReducer from "./userSlice";
+
+// persist 설정
+const persistConfig = {
+  key: "root", // 로컬 스토리지에 저장될 때의 key 값
+  storage: storage, // 사용할 스토리지 (local storage)
+};
+
+// 리듀서와 persist 설정을 결합
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    user: persistedReducer,
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: ["persist/PERSIST", "persist/REHYDRATE"], // persist 관련 액션 무시
+      },
+    }),
 });
