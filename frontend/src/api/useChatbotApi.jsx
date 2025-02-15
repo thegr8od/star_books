c4f9ccc62fc2cbcad3344c473a540fdd3659cda2
권동현 <ehdgus9370@gmail.com>
2025-02-15 17:02:59 +0900

    feat: chatbot api 추가

--- frontend/src/api/useChatbotApi.jsx
+++ frontend/src/api/useChatbotApi.jsx
@@ -1,1 +1,44 @@
+import { use } from "react";
 import useAxiosInstance from "./useAxiosInstance";
+
+const chat = (email, message, persona) => {
+    const jwt = localStorage.getItem("accessToken");
+
+    try {
+        const response = useAxiosInstance
+            .authApiClient(jwt)
+            .post(
+                '/chat/message',
+                {   
+                    email,
+                    message,
+                    persona
+                }
+            );
+
+        return response;
+    } catch(e) {
+        return e.response;
+    }
+}
+
+const getHistory = (email) => {
+    const jwt = localStorage.getItem("accessToken");
+
+    try {
+        const response = useAxiosInstance
+            .authApiClient(jwt)
+            .get(
+                `/chat/history?email=${email}`,
+            );
+
+        return response;
+    } catch(e) {
+        return e.response;
+    }
+}
+
+export default {
+    chat,
+    getHistory
+};
