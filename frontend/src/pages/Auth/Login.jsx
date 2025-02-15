82dc53a432398039b55650f5892a07064a959963
권동현 <ehdgus9370@gmail.com>
2025-02-15 21:53:53 +0900

    fix: config 수정

--- frontend/src/pages/Auth/Login.jsx
+++ frontend/src/pages/Auth/Login.jsx
@@ -1,11 +1,10 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import axios from "axios";
 import LoginModal from "./LoginModal";
 import CustomAlert from "../../components/CustomAlert";
 import VisibilityIcon from "@mui/icons-material/Visibility";
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff";
-import {loginMember} from "@api"
+import loginMember from "@api/useMemberApi"
 
 const Login = () => {
   const navigate = useNavigate();
