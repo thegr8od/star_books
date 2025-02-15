138acc516a380b3259d1ac1555981cb9e0fb197c
권동현 <ehdgus9370@gmail.com>
2025-02-15 16:47:48 +0900

    feat: uninverse api 추가

--- frontend/src/api/useUniverseApi.jsx
+++ frontend/src/api/useUniverseApi.jsx
@@ -1,1 +1,49 @@
 import useAxiosInstance from "./useAxiosInstance";
+
+//월별 나만의 우주 조회
+const getMonthlyPersonalUniv = (year, month) => {
+    const jwt = localStorage.getItem("accessToken");
+
+    try{
+        const response = useAxiosInstance
+            .authApiClient(jwt)
+            .get(`/universe/monthly/${year}/${month}`);
+        return response.data; 
+    } catch(e) {
+        return e.response;
+    }
+}
+
+//연도별 나만의 우주 조회
+const getYearlyPersonalUniv = (year) => {
+    const jwt = localStorage.getItem("accessToken");
+
+    try{
+        const response = useAxiosInstance
+            .authApiClient(jwt)
+            .get(`/universe/yearly/${year}`);
+        return response.data; 
+    } catch(e) {
+        return e.response;
+    }
+}
+
+// 특정 나만의 우주 조회
+const getPersonalUniv = (universeId) => {
+    const jwt = localStorage.getItem("accessToken");
+
+    try{
+        const response = useAxiosInstance
+            .authApiClient(jwt)
+            .get(`/universe/${universeId}`);
+        return response.data; 
+    } catch(e) {
+        return e.response;
+    }
+}
+
+export default{
+    getMonthlyPersonalUniv,
+    getYearlyPersonalUniv,
+    getPersonalUniv,
+}
