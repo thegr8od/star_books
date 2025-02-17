fc97ee65966bf25adf01463f459152dd94b0b622
권동현 <ehdgus9370@gmail.com>
2025-02-17 16:20:29 +0900

    feat: oauth redex 추가

--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -2,11 +2,27 @@ import { useEffect, useState } from "react";
 import { useSearchParams, useLocation } from "react-router-dom";
 import HomeBackground from "./HomeBackground";
 import LoginHome from "./LoginHome";
+import useMemberApi from "../../api/useMemberApi";
+//redux
+import { useDispatch } from "react-redux";
+import { setUser } from "../../store/userSlice";
 
 const Home = () => {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
   const [searchParams] = useSearchParams();
   const location = useLocation(); // 현재 페이지 경로 가져오기
+  const dispatch = useDispatch();
+
+  const getDetailData = async (jwt) => {
+    
+    try{
+      const response = useMemberApi.getUserInfo(jwt)
+      console.log(response);
+      return response.data;
+    } catch(e) {
+      console.log(e)
+    }
+  }
 
   useEffect(() => {
     const token = searchParams.get("token");
@@ -16,6 +32,13 @@ const Home = () => {
       // 토큰을 로컬 스토리지에 저장
       localStorage.setItem("accessToken", token);
       setIsLoggedIn(true); // 로그인 상태 업데이트
+
+      const data = getDetailData(token);
+      if(data.gender === 'OTHER'){
+        
+      } else {
+        dispatch(setUser({ ...data, isLogin: true }));
+      }
 
       window.location.replace("/");
     } else if (storedToken) {
