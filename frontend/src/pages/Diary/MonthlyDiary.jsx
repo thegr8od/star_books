c4b5305ee6bdb7602dae11ce7f57e7898742fc58
김은영 <kimeunyoung-0425@naver.com>
2025-02-19 19:29:09 +0900

    fix: 잘못된 일기 데이터가 있는 경우에 대한 에러 처리

--- frontend/src/pages/Diary/MonthlyDiary.jsx
+++ frontend/src/pages/Diary/MonthlyDiary.jsx
@@ -12,9 +12,9 @@ const MonthlyDiary = () => {
   const location = useLocation();
   const diaryRefs = useRef({});
 
-  const [diaries, setDiaries] = useState([]);
-  const [currentDate, setCurrentDate] = useState(new Date());
-  const selectedDate = location.state?.selectedDate;
+  const [diaries, setDiaries] = useState([]); // 작성한 일기 정보
+  const [currentDate, setCurrentDate] = useState(new Date()); // 날짜(년, 월)
+  const selectedDate = location.state?.selectedDate; // 선택된 날짜 (선택된 날짜가 있으면 스크롤)
 
   // 모달 상태 추가
   const [deleteModal, setDeleteModal] = useState({
@@ -22,7 +22,7 @@ const MonthlyDiary = () => {
     diaryToDelete: null,
   });
 
-  // axios (mount 될 때, currentDate가 변경될 때마다 실행)
+  // axios (mount 될 때, currentDate가 변경될 때마다 실행) -> 해당 월에 작성한 일기 정보 요청
   useEffect(() => {
     (async () => {
       const requestData = {
@@ -32,45 +32,7 @@ const MonthlyDiary = () => {
       const response = await useDiaryApi.getDiariesByMonth(requestData);
       if (response.status === 200) {
         console.log("일기 조회 성공");
-        const diaryDatas = response.data
-          .map((oneday) => {
-            if (!oneday) return null;
-
-            try {
-              const mappedData = {
-                id: oneday.diaryId,
-                date: Array.isArray(oneday.diaryDate)
-                  ? `${oneday.diaryDate[0]}-${String(
-                      oneday.diaryDate[1]
-                    ).padStart(2, "0")}-${String(oneday.diaryDate[2]).padStart(
-                      2,
-                      "0"
-                    )}`
-                  : oneday.diaryDate,
-                // DiaryEmotion이 있는 경우에만 색상과 감정값 설정
-                color: oneday.DiaryEmotion
-                  ? GetColor(
-                      oneday.DiaryEmotion.xValue,
-                      oneday.DiaryEmotion.yValue
-                    )
-                  : null,
-                emotions: oneday.DiaryEmotion
-                  ? {
-                      xValue: oneday.DiaryEmotion.xValue,
-                      yValue: oneday.DiaryEmotion.yValue,
-                    }
-                  : null,
-                content: oneday.content || "",
-                imageUrl: oneday.imageUrl || null,
-              };
-              return mappedData;
-            } catch (err) {
-              console.error("다이어리 데이터 변환 중 에러:", err);
-              return null;
-            }
-          })
-          .filter(Boolean); // null 값 제거
-        setDiaries(diaryDatas);
+        setDiaries(response.data);
       } else {
         console.log("일기 조회 실패");
       }
@@ -87,7 +49,7 @@ const MonthlyDiary = () => {
     }
   }, []);
 
-  // 수정 버튼 클릭 시
+  // 수정 버튼 클릭 시 -> 수정 페이지로 이동
   const handleEdit = (diary) => {
     navigate(`/diary/edit/${diary.diaryId}`, {
       state: {
@@ -96,7 +58,7 @@ const MonthlyDiary = () => {
     });
   };
 
-  // 삭제 버튼 클릭 시
+  // 삭제 버튼 클릭 시 -> 삭제 요청 후 성공시 정보 업데이트
   const handleDelete = async () => {
     const diary = deleteModal.diaryToDelete;
     const response = await useDiaryApi.deleteDiary(diary.diaryId);
@@ -115,43 +77,24 @@ const MonthlyDiary = () => {
     <Layout>
       <div className="flex flex-col h-[calc(100vh-4rem)]">
         <DiaryDate currentDate={currentDate} setCurrentDate={setCurrentDate} />
-        <div
-          className="flex-1 flex-col space-y-4 overflow-y-auto bg-neutral-100 rounded-3xl p-4"
-          style={{ scrollbarWidth: "none" }}
-        >
+        <div className="flex-1 flex-col space-y-4 overflow-y-auto bg-neutral-100 rounded-3xl p-4" style={{ scrollbarWidth: "none" }}>
           {diaries?.length ? (
             diaries.map((diary, index) => {
               // 요일 표시를 위해 Date 객체 생성
-              const diaryDateObj = new Date(
-                diary.diaryDate[0],
-                diary.diaryDate[1] - 1,
-                diary.diaryDate[2]
-              );
-              const formattedDate = `${diary.diaryDate[0]}-${String(
-                diary.diaryDate[1]
-              ).padStart(2, "0")}-${String(diary.diaryDate[2]).padStart(
-                2,
-                "0"
-              )}`;
-
+              const diaryDateObj = new Date(diary.diaryDate[0], diary.diaryDate[1] - 1, diary.diaryDate[2]);
+              const formattedDate = `${diary.diaryDate[0]}-${String(diary.diaryDate[1]).padStart(2, "0")}-${String(diary.diaryDate[2]).padStart(2, "0")}`;
               return (
-                <div
-                  key={diary.diaryId}
-                  ref={(el) => (diaryRefs.current[formattedDate] = el)}
-                  className={`space-y-3 px-5 py-3 bg-white rounded-xl shadow-sm ${
-                    selectedDate === formattedDate
-                      ? "animate-[pulse_1s_ease-in-out_1]"
-                      : ""
-                  }`}
-                >
+                <div key={diary.diaryId} ref={(el) => (diaryRefs.current[formattedDate] = el)} className={`space-y-3 px-5 py-3 bg-white rounded-xl shadow-sm ${selectedDate === formattedDate ? "animate-[pulse_1s_ease-in-out_1]" : ""}`}>
                   <div className="flex items-center justify-between">
                     <div className="flex space-x-3">
+                      {/* 감정 색 */}
                       <div
                         className="w-5 h-5 rounded-full"
                         style={{
-                          backgroundColor: diary.color,
+                          backgroundColor: GetColor(diary.DiaryEmotion?.xValue, diary.DiaryEmotion?.yValue),
                         }}
                       />
+                      {/* 날짜 */}
                       <div>
                         <p className="text-gray-600">
                           {diary.diaryDate[1]}월 {diary.diaryDate[2]}일
@@ -164,35 +107,28 @@ const MonthlyDiary = () => {
                       </div>
                     </div>
                     <div className="flex items-center space-x-4">
-                      <button
-                        onClick={() => handleEdit(diary)}
-                        className="text-gray-400 hover:text-gray-700"
-                      >
+                      {/* 수정 버튼 */}
+                      <button onClick={() => handleEdit(diary)} className="text-gray-400 hover:text-gray-700">
                         <Edit fontSize="inherit" />
                       </button>
-                      <button
-                        onClick={() =>
-                          setDeleteModal({ isOpen: true, diaryToDelete: diary })
-                        }
-                        className="text-gray-400 hover:text-gray-700"
-                      >
+                      {/* 삭제 버튼 */}
+                      <button onClick={() => setDeleteModal({ isOpen: true, diaryToDelete: diary })} className="text-gray-400 hover:text-gray-700">
                         <Delete fontSize="inherit" />
                       </button>
                     </div>
                   </div>
 
+                  {/* 내용 */}
                   <p className="text-sm">{diary.content}</p>
 
+                  {/* 사진 */}
                   {diary.imageUrl && (
                     <div>
-                      <img
-                        src={diary.imageUrl}
-                        alt="Diary img"
-                        className="object-cover rounded-lg"
-                      />
+                      <img src={diary.imageUrl} alt="Diary img" className="object-cover rounded-lg" />
                     </div>
                   )}
 
+                  {/* 해시태그 */}
                   <div className="flex flex-wrap gap-2">
                     {diary.hashtags.map((tag, index) => (
                       <span key={index} className="text-xs text-gray-400">
@@ -214,12 +150,7 @@ const MonthlyDiary = () => {
       </div>
 
       {/* 모달 추가 */}
-      <ConfirmModal
-        isOpen={deleteModal.isOpen}
-        onClose={() => setDeleteModal({ isOpen: false, diaryToDelete: null })}
-        onConfirm={handleDelete}
-        title="잠시만요"
-      />
+      <ConfirmModal isOpen={deleteModal.isOpen} onClose={() => setDeleteModal({ isOpen: false, diaryToDelete: null })} onConfirm={handleDelete} title="잠시만요" />
     </Layout>
   );
 };
